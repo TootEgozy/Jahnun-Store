e754f344219baa52ee8a794ef79f87984e6c2327
Toot Egozy <strawberevil@gmail.com>
2021-05-03 22:41:50 +0300

    more client testing

--- jahnun-client/src/App.js
+++ jahnun-client/src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
-const hey = "hey";
+import { Route } from 'react-router';
+import User from './components/user';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  console.log(hey);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Toot's App
-        </p>
-  
-      </header>
+    <div>
+      <BrowserRouter>
+        <Route exact path='/' component={User} />
+      </BrowserRouter>
+
     </div>
   );
 }
 
 export default App;
+
