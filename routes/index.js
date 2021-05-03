5aa229e516c14b645c7f0dcadd66af07f9fc7188
Toot Egozy <strawberevil@gmail.com>
2021-05-03 14:07:28 +0300

    test client

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.use('/user', require('./user.route'));
 router.use('/dish', require('./dish.route'));
 router.use('/order', require('./order.route'));
 
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../jahnun-client/public", "index.html"));
-});
+// router.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../jahnun-client/public", "index.html"));
+// });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,23 +12,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//app.use(require('./routes'));
+app.use(require('./routes'));
 
 if (process.env.NODE_ENV === 'production') {
 
     // Exprees will serve up production assets
     app.use(express.static('client/build'));
   
-//     // Express serve up index.html file if it doesn't recognize route
-//     const path = require('path');
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, './jahnun-client/public', 'build', 'index.html'));
-//     });
+     // Express serve up index.html file if it doesn't recognize route
+     const path = require('path');
+     app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, './jahnun-client/public', 'build', 'index.html'));
+     });
    }
 
-  app.get('/', (req, res)=>{
-      res.send("hey");
-  })
+//   app.get('/', (req, res)=>{
+//       res.send("hey");
+//   })
 
 
 //connect to db with mongoose
