a642c08fd09ccfb6ba40d9fe16b8d371345be49d
Toot Egozy <strawberevil@gmail.com>
2021-05-03 23:04:59 +0300

    client test

--- jahnun-client/src/App.js
+++ jahnun-client/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { Route } from 'react-router';
+import User from './components/user.component';
+import { BrowserRouter } from 'react-router-dom';
 
 const hey = "hey";
 
