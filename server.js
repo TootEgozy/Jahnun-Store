39ae02df01fdf2a11f6128595d624aeb24cb581e
Toot Egozy <strawberevil@gmail.com>
2021-05-03 22:16:57 +0300

    another client debug

--- server.js
+++ server.js
@@ -19,10 +19,14 @@ if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
     app.use(express.static('client/build'));
   
+    app.get('/user', (req, res)=>{
+        res.send("in user");
+    })
      // Express serve up index.html file if it doesn't recognize route
      const path = require('path');
      app.get('*', (req, res) => {
-       res.sendFile(path.resolve(__dirname, './jahnun-client/public', 'build', 'index.html'));
+        console.log(path.resolve(__dirname, './jahnun-client/public'));
+        res.sendFile(path.resolve(__dirname, './jahnun-client/public', 'build', 'index.html'));
      });
    }
 
