8cc6965f61fb16b7a808f63de83bf2b131502089
Toot Egozy <strawberevil@gmail.com>
2021-05-05 14:00:24 +0300

    database routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 
-router.use('/user', require('./user.route'));
-router.use('/dish', require('./dish.route'));
-router.use('/order', require('./order.route'));
+router.use('api/user', require('./user.route'));
+router.use('api/dish', require('./dish.route'));
+router.use('api/order', require('./order.route'));
 
 // router.get("/", (req, res) => {
 //     res.sendFile(path.join(__dirname, "../jahnun-client/public", "index.html"));
